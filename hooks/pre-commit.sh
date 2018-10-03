b85c15ebbfcc908aa87f6a13d0fac2d787e91437
Josep Maria Bernabé Gisbert <jbgisbert@kumori.systems>
2018-10-03 12:46:05 +0200

    Add the pre-commit hook to compile all ts files before commiting

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_before="$(git status)"
+npm run build
+_after="$(git status)"
+export COLOR_GREEN='\e[0;32m'
+
+if [ "$_before" != "$_after" ]; then
+  git add lib/*
+  echo "Source files recompiled and new files staged for this commit"
+fi
